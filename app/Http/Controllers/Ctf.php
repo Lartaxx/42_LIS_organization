4942b7f35250ce77f12e7ba7ad2d53e8653eb80e
Lartaxx <didou.com@gmail.com>
2023-04-27 23:16:59 +0200

    Add ctf post

--- app/Http/Controllers/Ctf.php
+++ app/Http/Controllers/Ctf.php
@@ -20,7 +20,12 @@ class Ctf extends Controller
     protected function ctf_post() {
         $initFlag = Flags::where("user_id", auth()->user()->id)->first();
         if (is_null($initFlag)) {
-
+            Flags::create([
+                "user_id" => auth()->user()->id,
+                "flag_init" => true
+            ]);
+            return redirect()->route("profile")->with("success", "Votre aventure commence maintenan, bonne chance !");
         }
+        return redirect()->route("profile")->with("error", "Vous avez déjà initialisé votre aventure !");
     }
 }
